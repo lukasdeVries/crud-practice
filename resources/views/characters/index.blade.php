dab7ac48aac2a9c0d0c722b58d2e50e12f9766b8
lukasdeVries <lukaspdevries@gmail.com>
2024-04-04 11:14:36 +0200

    character index page formating

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -9,14 +9,16 @@
             </div>
             <div class="page__grid">
                 @foreach ($characters as $character)
-                <div class="character">
-                    <div class="character__info">
-                        <p class="character__name">{{ $character->name }}</p>
-                        <p class="character__description">{{ $character->description }}</p>
-                        <p class="character__country">{{ $character->country->name }}</p>
+                    <div class="character">
+                        <div class="character__info">
+                            <p class="character__name">{{ $character->name }}</p>
+                            <p class="character__description">{{ $character->description }}</p>
+                            <p class="character__country">{{ $character->country->name }}</p>
+                        </div>
+                        <a class="edit" href="{{ route('character.edit', $character->id) }}">
+                            <span class="material-symbols-outlined">edit</span>
+                        </a>
                     </div>
-                    <a class="edit" href="{{ route('character.edit', $character->id) }}"><span class="material-symbols-outlined">edit</span></a>
-                </div>
                 @endforeach
             </div>
         </div>
